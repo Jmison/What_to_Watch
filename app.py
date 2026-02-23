f94b5ed86f0e31549e7c9df9c4d84610d7b5a7a5
jmison <jnixonrva@gmail.com>
2026-02-23 17:08:47 -0500

    created two new functions

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/ping')
+@app.route('/ping/')
 def ping():
     return jsonify(ok = True), 200
 
@@ -17,6 +18,23 @@ def ping():
 def echo():
     q = request.args.get("q", "")
     return jsonify(q = q), 200
+
+# checks and sees if the API information is present, returns false if not.
+@app.route('/debug/env')
+def debug():
+    list_of_req = {
+        "api_key": bool(os.getenv("MOVIEGLU_API_KEY")),
+        "client": bool(os.getenv("MOVIEGLU_CLIENT")),
+        "auth": bool(os.getenv("MOVIEGLU_AUTH")),
+        "territory": bool(os.getenv("MOVIEGLU_TERRITORY")),
+        "api_base": bool(os.getenv("MOVIEGLU_API_BASE"))
+    }
+    return jsonify(list_of_req), 200
+
+# first real end-point grabbing from MOVIEGLU
+@app.route('/movie-details')
+def movie_details():
+    return jsonify("placeholder: text"), 200 #placeholders
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
