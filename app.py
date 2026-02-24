449da7c5e8ce55e12011ea8ba8abac3c1fc65815
jmison <jnixonrva@gmail.com>
2026-02-24 17:05:56 -0500

    added new functions

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify, request
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timezone
+
+import requests
 load_dotenv()
 
 app = Flask(__name__)
@@ -26,7 +28,7 @@ def debug():
     list_of_req = {
         "api_key": bool(os.getenv("MOVIEGLU_API_KEY")),
         "client": bool(os.getenv("MOVIEGLU_CLIENT")),
-        "auth": bool(os.getenv("MOVIEGLU_AUTH")),``
+        "auth": bool(os.getenv("MOVIEGLU_AUTH")),
         "territory": bool(os.getenv("MOVIEGLU_TERRITORY")),
         "api_base": bool(os.getenv("MOVIEGLU_API_BASE")),
         "api_version": bool(os.getenv("MOVIEGLU_API_VERSION")),
@@ -51,6 +53,35 @@ def movieglu_headers():
 def film_details():
     header_list = list(movieglu_headers().keys())
     return jsonify(header_list), 200 
+
+@app.route('/filmLiveSearch/', methods =["GET"])  # type: ignore
+def film_live_search():
+    query = request.args.get("query", "")
+    if query == "":
+        return {
+            "error": "query required"
+        }, 400
+    
+    get_base = os.getenv("MOVIEGLU_API_BASE") 
+    build_url = str(get_base) + "/filmLiveSearch/"
+    params = {"query": query}
+    headers = movieglu_headers()
+    response = requests.get(build_url, headers = headers, params=params)
+
+    if response.status_code != 200:
+        return {
+            "response" :response.status_code, 
+            # "body": response.json(),
+            "content type": response.headers.get("Content-Type"),
+            "text preview": response.text[:200],
+            "url": response.url,
+            "raw body len": len(response.content),
+            "MG Message": response.headers.get("MG-message"),
+            "MG-error": response.headers.get("MG-error"),
+            "has auth": bool(headers.get("Authorization"))
+        }
+    if response.status_code == 200:
+        return response.json(), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
